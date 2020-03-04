523663bd5358d0ac16727fee5563802465d4f6de
khcho902 <khcho902@gmail.com>
2020-03-04 15:49:59 +0900

    modified ft_strtrim (added ft_substr to ft_strtrim)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: kycho <kycho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/29 17:41:42 by kycho             #+#    #+#             */
-/*   Updated: 2020/02/29 19:01:11 by kycho            ###   ########.fr       */
+/*   Updated: 2020/03/04 15:47:16 by kycho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,24 +29,18 @@ static int	is_in_char_set(char c, char const *set)
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*trimed_str;
-	size_t	trimed_len;
 	size_t	start_idx;
 	size_t	end_idx;
 
-	trimed_len = 0;
 	start_idx = 0;
 	end_idx = ft_strlen(s1) - 1;
 	while (is_in_char_set(s1[start_idx], set) && s1[start_idx] != '\0')
 		start_idx++;
 	if (s1[start_idx] != '\0')
-	{
 		while (is_in_char_set(s1[end_idx], set))
 			end_idx--;
-		trimed_len = end_idx - start_idx + 1;
-	}
-	trimed_str = (char *)malloc(sizeof(char)*(trimed_len + 1));
+	trimed_str = ft_substr(s1, start_idx, end_idx - start_idx + 1);
 	if (trimed_str == NULL)
 		return (NULL);
-	ft_strlcpy(trimed_str, s1 + start_idx, trimed_len + 1);
 	return (trimed_str);
 }
