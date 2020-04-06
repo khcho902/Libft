ae60f89ec4e5a72d0f5831012599ddde14d47a52
Kyunghyun Cho <kycho@c4r6s8.42seoul.kr>
2020-04-06 14:06:49 +0900

    change malloc -> calloc in ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kycho <kycho@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/29 16:48:20 by kycho             #+#    #+#             */
-/*   Updated: 2020/04/04 18:51:16 by kycho            ###   ########.fr       */
+/*   Updated: 2020/04/06 14:05:25 by kycho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	joined_len;
 
 	joined_len = ft_strlen(s1) + ft_strlen(s2);
-	joined_str = (char *)malloc(sizeof(char) * (joined_len + 1));
+	joined_str = (char *)ft_calloc(joined_len + 1, sizeof(char));
 	if (joined_str == NULL)
 		return (NULL);
 	ft_strlcat(joined_str, s1, joined_len + 1);
